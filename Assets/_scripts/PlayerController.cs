ca3166e3c9d987b5ed2d72158dd220a39e83d587
kbarry <g00339811@gmit.ie>
2018-10-05 03:08:12 +0100

    Added jump to player

---
+++ Assets/_scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	// 
+	Rigidbody2D rigid2D;
+
+	//player variables
+	public float moveSpeed;
+	public float jumpHeight;
+
+	// Use this for initialization
+	void Start () {
+		rigid2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+			rigid2D.velocity = new Vector2(0,jumpHeight);
+
+		}	
+	}
+}
