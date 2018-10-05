0ab3ff5d7d32922d25d76fd3dbc30edac2909d7a
kbarry <g00339811@gmit.ie>
2018-10-05 21:22:54 +0100

    movement adjusted

--- Assets/_scripts/PlayerController.cs
+++ Assets/_scripts/PlayerController.cs
@@ -7,20 +7,40 @@ public class PlayerController : MonoBehaviour {
 	Rigidbody2D rigid2D;
 
 	//player variables
-	public float moveSpeed;
-	public float jumpHeight;
+	public float moveSpeed = 5;
+	public float maxSpeed = 2f;
+	public float jumpHeight = 18;
 
 	// Use this for initialization
 	void Start () {
-		rigid2D = GetComponent<Rigidbody2D>();
+		rigid2D = GetComponent<Rigidbody2D> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
-		if(Input.GetKeyDown(KeyCode.Space)){
-			rigid2D.velocity = new Vector2(0,jumpHeight);
-
-		}	
 	}
+
+	void FixedUpdate () {
+		Move (Input.GetAxis ("Horizontal"));
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Jump();
+			//
+		}
+	}
+
+	// Move player
+	public void Move (float horizontalInput) {
+		Vector2 moveVel = rigid2D.velocity;
+		moveVel.x = horizontalInput * moveSpeed;
+		rigid2D.velocity = moveVel;
+	}
+
+	// Jump 
+	public void Jump () {
+		rigid2D.velocity = new Vector2 (moveSpeed, jumpHeight);
+		//
+
+	}
+
 }
