482dd4be519ab26d8313e4b22a9510badbbbd48c
kevin barry <g00339811@gmit.ie>
2018-10-22 23:50:35 +0100

    Added Particle Effects

---
+++ Assets/_scripts/DestroyFinishedParticle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+/// <summary>
+/// DestroyFinishedParticle destroys objects to conserve memory
+/// </summary>
+public class DestroyFinishedParticle : MonoBehaviour
+{
+    private ParticleSystem thisParticleSystem;
+    // Use this for initialization
+    void Start()
+    {
+        thisParticleSystem = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (thisParticleSystem.isPlaying)
+        {
+            return;
+        }
+        Destroy(gameObject);
+    }
+}
--- Assets/_scripts/LevelManager.cs
+++ Assets/_scripts/LevelManager.cs
@@ -11,8 +11,12 @@ public class LevelManager : MonoBehaviour {
 	/// The current checkpoint.
 	/// </summary>
 	public GameObject currentCheckpoint;
-
-	/// <summary>
+  
+    public GameObject deathParticle;
+    public GameObject respawnParticle;
+    public float respawnDelay;
+    
+    /// <summary>
 	/// The player.
 	/// </summary>
 	private PlayerController player;
@@ -29,11 +33,35 @@ public class LevelManager : MonoBehaviour {
 		
 	}
 
+    
 	/*
 	*/
 	public void RespawnPlayer(){
-		Debug.Log("DEBUG : Player Respawn");
-		player.transform.position = currentCheckpoint.transform.position;
-	}
+        StartCoroutine("RespawnPlayerCo");
+    }
+    /// <summary>
+    /// RespawnPlayerCo adds a delay to the player respawn
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator RespawnPlayerCo()
+    {
+        Debug.Log("DEBUG : Player Respawn");
 
+        // Instantiate death particle where player died
+        Instantiate(deathParticle, player.transform.position, player.transform.rotation);
+
+        // Disable the player while respawning
+        player.enabled = false;
+        player.GetComponent<Renderer>().enabled = false;
+
+        yield return new WaitForSeconds(respawnDelay);
+        player.transform.position = currentCheckpoint.transform.position;
+
+        // Re enable player
+        player.enabled = true;
+        player.GetComponent<Renderer>().enabled = true;
+        // Instantiate respawn particle where player respawns
+        Instantiate(respawnParticle, currentCheckpoint.transform.position, currentCheckpoint.transform.rotation);
+
+    }
 }
