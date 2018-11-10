aeb4a15801db5350e89ad769be735cd8ce631478
kbarry <g00339811@gmit.ie>
2018-11-10 05:10:10 +0000

    Main Menu set up

---
+++ Assets/_scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour {
+
+    public string beginLevel;
+    public string selectLevel;
+
+    // New Game
+    public void NewGame()
+    {
+        // Load first level
+        SceneManager.LoadScene(beginLevel);
+    }
+    // Select Level
+    public void SelectLevel()
+    {
+        // Load first level
+        SceneManager.LoadScene(selectLevel);
+    }
+    // exit Game
+    public void ExitGame()
+    {
+        // Quit Game
+        //SceneManager.LoadScene(beginLevel);
+        Application.Quit();
+        Debug.Log("User has exited game");
+    }
+
+}
