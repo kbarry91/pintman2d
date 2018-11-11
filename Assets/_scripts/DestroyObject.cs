b5e966b0c6014ae2d594213272ad8770be329164
kevin barry <g00339811@gmit.ie>
2018-11-11 01:26:24 +0000

    Added script to destroy unused objects

---
+++ Assets/_scripts/DestroyObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour {
+
+    public float timeSpan;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeSpan -= Time.deltaTime;
+
+        // Destroy unused object
+        if (timeSpan < 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
